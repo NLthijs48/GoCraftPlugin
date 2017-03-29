417b56b37dbb80e330bfb9883cd676d702979d08
Thijs Wiefferink <thijs@wiefferink.me>
2017-03-29 22:42:42 +0200

    Add /votetop to replace other votetop plugin
    Still need to override the scoreboard and ditch the other votetop

--- Go-Craft Core/src/main/java/me/wiefferink/gocraft/information/providers/VoteInfo.java
+++ Go-Craft Core/src/main/java/me/wiefferink/gocraft/information/providers/VoteInfo.java
@@ -4,9 +4,8 @@ import me.wiefferink.gocraft.information.InformationProvider;
 import me.wiefferink.gocraft.information.InformationRequest;
 import me.wiefferink.gocraft.sessions.GCPlayer;
 import me.wiefferink.gocraft.tools.storage.Database;
+import me.wiefferink.gocraft.votes.VoteManager;
 import me.wiefferink.interactivemessenger.processing.Message;
-
-import java.util.Calendar;
 
 public class VoteInfo extends InformationProvider {
 
@@ -16,30 +15,18 @@ public class VoteInfo extends InformationProvider {
 			GCPlayer gcPlayer = Database.getPlayer(request.getAbout().getUniqueId());
 
 			// Votes in total
-			Long totalVotes = (Long)session.createQuery("SELECT count(*) FROM Vote WHERE gcPlayer = :player")
+			long totalVotes = (long)session.createQuery("SELECT count(*) FROM Vote WHERE gcPlayer = :player")
 					.setParameter("player", gcPlayer)
 					.uniqueResult();
-			if(totalVotes != null && totalVotes > 0) {
+			if(totalVotes > 0) {
 				request.message(Message.fromKey("information-totalVotes").replacements(totalVotes));
 			}
 
 			// Votes this month
-			Calendar monthStart = Calendar.getInstance();
-			monthStart.set(Calendar.DAY_OF_MONTH,	monthStart.getActualMinimum(Calendar.DAY_OF_MONTH));
-			monthStart.set(Calendar.HOUR_OF_DAY, 0);
-			monthStart.set(Calendar.MINUTE, 0);
-			monthStart.set(Calendar.SECOND, 0);
-			monthStart.set(Calendar.MILLISECOND, 0);
-			Calendar monthEnd = Calendar.getInstance();
-			monthEnd.set(Calendar.DAY_OF_MONTH,	monthEnd.getActualMaximum(Calendar.DAY_OF_MONTH));
-			monthEnd.set(Calendar.HOUR_OF_DAY, 23);
-			monthEnd.set(Calendar.MINUTE, 59);
-			monthEnd.set(Calendar.SECOND, 59);
-			monthEnd.set(Calendar.MILLISECOND, 999);
-			Long monthVotes = (Long)session.createQuery("SELECT count(*) from Vote WHERE gcPlayer = :player AND at < :monthEnd AND at >= :monthStart")
+			long monthVotes = (long)session.createQuery("SELECT count(*) from Vote WHERE gcPlayer = :player AND at < :monthEnd AND at >= :monthStart")
 					.setParameter("player", gcPlayer)
-					.setParameter("monthEnd", monthEnd.getTime())
-					.setParameter("monthStart", monthStart.getTime())
+					.setParameter("monthEnd", VoteManager.getMonthEnd().getTime())
+					.setParameter("monthStart", VoteManager.getMonthStart().getTime())
 					.uniqueResult();
 			request.message(Message.fromKey("information-monthVotes").replacements(monthVotes));
 		});
--- Go-Craft Core/src/main/java/me/wiefferink/gocraft/votes/VoteManager.java
+++ Go-Craft Core/src/main/java/me/wiefferink/gocraft/votes/VoteManager.java
@@ -4,15 +4,27 @@ import com.vexsoftware.votifier.model.VotifierEvent;
 import me.wiefferink.gocraft.GoCraft;
 import me.wiefferink.gocraft.features.Feature;
 import me.wiefferink.gocraft.sessions.GCPlayer;
+import me.wiefferink.gocraft.tools.PageDisplay;
 import me.wiefferink.gocraft.tools.storage.Database;
+import me.wiefferink.interactivemessenger.processing.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
+import org.bukkit.permissions.PermissionDefault;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class VoteManager extends Feature {
 
 	public VoteManager() {
 		listen("collectVotes");
+		command("votetop", "Vote top 10", "/votetop [page]");
+		permission("votetop", "Access to the /votetop command", PermissionDefault.TRUE);
 	}
 
 	@EventHandler
@@ -36,6 +48,86 @@ public class VoteManager extends Feature {
 				GoCraft.debug("Received vote:", vote);
 			})
 		);
+	}
+
+	@Override
+	public void onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!sender.hasPermission("gocraft.votetop")) {
+			plugin.message(sender, "votetop-noPermission");
+			return;
+		}
+
+		async(() ->
+			database(session -> {
+				Date monthStart = VoteManager.getMonthStart().getTime();
+				Date monthEnd = VoteManager.getMonthEnd().getTime();
+
+				// Count players that voted in this month
+				long playerCount = (long)session.createQuery("SELECT count(distinct gcPlayer_id) from Vote WHERE at < :monthEnd AND at >= :monthStart")
+						.setParameter("monthStart", monthStart)
+						.setParameter("monthEnd", monthEnd)
+						.uniqueResult();
+
+				PageDisplay display = new PageDisplay(sender, (int)playerCount, "/votetop") {
+
+					@Override
+					public Message renderHeader() {
+						return Message.fromKey("votetop-header").prefix();
+					}
+
+					@Override
+					public Message renderEmpty() {
+						return Message.fromKey("votetop-empty").prefix();
+					}
+
+					@Override
+					public boolean renderItems(int itemStart, int itemEnd) {
+						// Fetch the players and vote counts
+						// TODO check if we can prevent a select per gcPlayer
+						@SuppressWarnings("unchecked")
+						List<Map<String,Object>> playerVoteCounts = session.createQuery(
+								"SELECT new map(gcPlayer as gcPlayer, count(*) as votes) " +
+										"FROM Vote " +
+										"WHERE at < :monthEnd AND at >= :monthStart " +
+										"GROUP BY gcPlayer_id " +
+										"ORDER BY count(*) DESC")
+								.setParameter("monthStart", monthStart)
+								.setParameter("monthEnd", monthEnd)
+								.setMaxResults(itemEnd - itemStart + 1)
+								.setFirstResult(itemStart).getResultList();
+						// TODO make result more pretty and typesafe
+						int index = 1;
+						for(Map<String, Object> playerAndCount : playerVoteCounts) {
+							message(Message.fromKey("votetop-item").replacements(index, ((GCPlayer)playerAndCount.get("gcPlayer")).getPlayerName(), playerAndCount.get("votes")));
+							index++;
+						}
+						return true;
+					}
+				}.renderPage(args.length > 0 ? args[0] : null);
+
+				sync(display::show);
+			})
+		);
+	}
+
+	public static Calendar getMonthStart() {
+		Calendar result = Calendar.getInstance();
+		result.set(Calendar.DAY_OF_MONTH, result.getActualMinimum(Calendar.DAY_OF_MONTH));
+		result.set(Calendar.HOUR_OF_DAY, 0);
+		result.set(Calendar.MINUTE, 0);
+		result.set(Calendar.SECOND, 0);
+		result.set(Calendar.MILLISECOND, 0);
+		return result;
+	}
+
+	public static Calendar getMonthEnd() {
+		Calendar result = Calendar.getInstance();
+		result.set(Calendar.DAY_OF_MONTH, result.getActualMaximum(Calendar.DAY_OF_MONTH));
+		result.set(Calendar.HOUR_OF_DAY, 23);
+		result.set(Calendar.MINUTE, 59);
+		result.set(Calendar.SECOND, 59);
+		result.set(Calendar.MILLISECOND, 999);
+		return result;
 	}
 
 }
