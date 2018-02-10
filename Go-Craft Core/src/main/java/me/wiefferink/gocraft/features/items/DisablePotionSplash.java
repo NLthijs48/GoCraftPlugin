ff203d5ef9c0a7cd550ca2305c65774abce1fb5e
Thijs Wiefferink <thijs@wiefferink.me>
2018-02-10 22:03:17 +0100

    Fix disabling potion splash for lingering potions

--- Go-Craft Core/src/main/java/me/wiefferink/gocraft/features/items/DisablePotionSplash.java
+++ Go-Craft Core/src/main/java/me/wiefferink/gocraft/features/items/DisablePotionSplash.java
@@ -2,6 +2,7 @@ package me.wiefferink.gocraft.features.items;
 
 import me.wiefferink.gocraft.features.Feature;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.LingeringPotionSplashEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 
 public class DisablePotionSplash extends Feature {
@@ -13,6 +14,14 @@ public class DisablePotionSplash extends Feature {
 	// Block effect when a splash potion hits the ground
 	@EventHandler(ignoreCancelled = true)
 	public void onPotionSplash(PotionSplashEvent event) {
+		if(inWorld(event)) {
+			event.setCancelled(true);
+		}
+	}
+
+	// Block effect when a splash lingering potion hits the ground
+	@EventHandler(ignoreCancelled = true)
+	public void onLingeringPotionSplash(LingeringPotionSplashEvent event) {
 		if(inWorld(event)) {
 			event.setCancelled(true);
 		}
