fc8f5ded4af58e7513ea33c4cd23f065f4554402
Thijs Wiefferink <thijs@wiefferink.me>
2018-02-10 22:04:29 +0100

    Fix disabling potion throw for lingering potions

--- Go-Craft Core/src/main/java/me/wiefferink/gocraft/features/items/DisablePotionThrow.java
+++ Go-Craft Core/src/main/java/me/wiefferink/gocraft/features/items/DisablePotionThrow.java
@@ -1,6 +1,7 @@
 package me.wiefferink.gocraft.features.items;
 
 import me.wiefferink.gocraft.features.Feature;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -14,11 +15,10 @@ public class DisablePotionThrow extends Feature {
 	// Prevent Potion throw
 	@EventHandler(ignoreCancelled = true)
 	public void onItemUse(PlayerInteractEvent event) {
-		int potionData = event.getPlayer().getItemInHand().getDurability() % 32768;
 		if(inWorld(event)
-				&& (event.getAction() == Action.RIGHT_CLICK_AIR
-				|| event.getAction() == Action.RIGHT_CLICK_BLOCK)
-				&& potionData / 16384 > 0) {
+				&& (event.getMaterial() == Material.SPLASH_POTION || event.getMaterial() == Material.LINGERING_POTION)
+				&& (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
+				) {
 			event.setCancelled(true);
 		}
 	}
